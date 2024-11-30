652da3eed38fb13bedda992ffcb4ae7220491384
hiaoxui <hiaoxui@gmail.com>
2024-11-30 14:45:30 -0500

    add node admin

--- walless/main/admin.py
+++ walless/main/admin.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.Push)
+class NodeAdmin(admin.ModelAdmin):
+    ordering = ['node_id']  # Sort by 'node_id' in ascending order
+    list_filter = ['deleted', 'hidden', 'tag']  # Add filters to the right side
 
+
+admin.site.register(models.Push)
 admin.site.register(models.User)
-admin.site.register(models.Node)
+admin.site.register(models.Node, NodeAdmin)
 admin.site.register(models.Relay)
 admin.site.register(models.Probe)
 admin.site.register(models.Registration)
