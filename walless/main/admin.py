683df2254026b1fa11ab30c9b64332cddd8c50ba
hiaoxui <hiaoxui@gmail.com>
2024-11-30 15:17:39 -0500

    add user admin

--- walless/main/admin.py
+++ walless/main/admin.py
@@ -5,10 +5,17 @@ from . import models
 class NodeAdmin(admin.ModelAdmin):
     ordering = ['node_id']  # Sort by 'node_id' in ascending order
     list_filter = ['deleted', 'hidden', 'tag']  # Add filters to the right side
+    list_display = ['name', 'node_id', 'idc', 'tag', 'hidden']
+
+
+class UserAdmin(admin.ModelAdmin):
+    ordering = ['user_id']
+    list_filter = ['tag', 'enabled', ]
+    list_display = ['email', 'user_id', 'tag', 'enabled', 'remarks']
 
 
 admin.site.register(models.Push)
-admin.site.register(models.User)
+admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Node, NodeAdmin)
 admin.site.register(models.Relay)
 admin.site.register(models.Probe)
