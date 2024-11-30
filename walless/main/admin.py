d38591fc88a4a1e1ff88ccf027fcbdd2f9f9b253
hiaoxui <hiaoxui@gmail.com>
2024-11-30 15:24:33 -0500

    more admins

--- walless/main/admin.py
+++ walless/main/admin.py
@@ -10,17 +10,27 @@ class NodeAdmin(admin.ModelAdmin):
 
 class UserAdmin(admin.ModelAdmin):
     ordering = ['user_id']
-    list_filter = ['tag', 'enabled', ]
+    list_filter = ['tag', 'enabled']
     list_display = ['email', 'user_id', 'tag', 'enabled', 'remarks']
+    search_fields = ['email', 'user_id']
+
+
+class RelayAdmin(admin.ModelAdmin):
+    ordering = ['relay_id']
+    list_display = ['relay_id', 'source', 'target']
+
+
+class MixAdmin(admin.ModelAdmin):
+    list_display = ['id', 'source', 'target']
 
 
 admin.site.register(models.Push)
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Node, NodeAdmin)
-admin.site.register(models.Relay)
-admin.site.register(models.Probe)
-admin.site.register(models.Registration)
-admin.site.register(models.Sublog)
-admin.site.register(models.Traffic)
-admin.site.register(models.TrafficLog)
-admin.site.register(models.Mix)
+admin.site.register(models.Relay, RelayAdmin)
+admin.site.register(models.Mix, MixAdmin)
+# admin.site.register(models.Probe)
+# admin.site.register(models.Registration)
+# admin.site.register(models.Sublog)
+# admin.site.register(models.Traffic)
+# admin.site.register(models.TrafficLog)
